96e3f3589bc30a4d9eb436f8c83bb614bfec191b
Jonathan Hegranes <hegranes@gmail.com>
2014-11-07 15:26:40 -0800

    updated outlets and actions

--- Lemonade/ViewController.swift
+++ Lemonade/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var walletLabel: UILabel!
+    @IBOutlet weak var lemonSuppliesLabel: UILabel!
+    @IBOutlet weak var sugarCubesSuppliesLabel: UILabel!
+    @IBOutlet weak var lemonsMixLabel: UILabel!
+    @IBOutlet weak var sugarCubesMixLabel: UILabel!
+    
+    //Key Variables
+    var wallet = 10
+    var lemons = 1
+    var sugarCubes = 1
+    var lemonsCost = 2
+    var sugarCubesCost = 1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +35,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // Start the Day Simulation
+    @IBAction func startTheDayButtonPressed(sender: AnyObject) {
+    }
+    
+    //Shopping Actions
+    @IBAction func addLemonsButtonPressed(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func removeLemonsButtonPressed(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func addSugarCubesButtonPressed(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func removeSugarCubesButtonPressed(sender: AnyObject) {
+    }
+    
+    
+    //Mixing Actions
+    @IBAction func addLemonsToMixButtonPressed(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func removeLemonsFromMixButtonPressed(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func addSugarCubesToMixButtonPressed(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func removeSugarCubesFromMixButtonPressed(sender: AnyObject) {
+    }
+    
+    
+    //Helper Functions
+    
+    
 
 }
 
